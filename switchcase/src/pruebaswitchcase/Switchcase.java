ff6cc090589182f7bab25d58adb980809a2fa685
incluitmarilyn2019 <andreamacuna@gmail.com>
2019-05-08 13:53:55 -0300

    proyecto

---
+++ switchcase/src/pruebaswitchcase/Switchcase.java
@@ -0,0 +1,78 @@
+package pruebaswitchcase;
+
+public class Switchcase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       // int y char
+		//int x;
+		//x=0;
+		//switch (x)
+	//	{
+		//case 0:
+	//	{
+		//	System.out.println("Esta es la opcion 0");
+			//break;
+		//}
+	//	case 1:
+		//{
+			//System.out.println("Esta es la opcion 1");
+	//		break;
+	//	}
+	//}
+		int x;
+		x=1;
+		switch (x)
+		{
+		case 0:
+		{
+			System.out.println("Esta es la opcion 0");
+			break;
+	}
+		case 1:
+		{
+			System.out.println("Esta es la opcion 1");
+			break;
+		}
+	}
+		//char x;
+		//x='a';
+	//	switch (x)
+	//	{
+		//case 'a':
+		//{
+		//	System.out.println("Esta es la opcion 0");
+		//	break;
+	//	}
+		//case 'b':
+		//{
+			//System.out.println("Esta es la opcion 1");
+		//break;
+	//	}
+		//}
+	
+		//char x;
+	//	x='B';
+	//	switch (x)
+	//	{
+	//	case 'a':
+	//	{
+		//	System.out.println("Esta es la opcion 0");
+			//break;
+		//}
+	//	case 'b':
+		//{
+		//	System.out.println("Esta es la opcion 1");
+		//	break;
+		//}
+	//	default:
+		//{
+		//	System.out.println("esta opcion es la opcion por defecto");
+	//	}
+	//}
+}
+	}
+
+
+
+
